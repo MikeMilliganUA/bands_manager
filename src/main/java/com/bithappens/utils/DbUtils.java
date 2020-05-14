303ac9110af6187661691b246c8a81faedd7050e
bit <mikemilligan.ua@gmail.com>
2020-05-14 20:27:00 +0300

    modify connection class

--- src/main/java/com/bithappens/utils/DbUtils.java
+++ src/main/java/com/bithappens/utils/DbUtils.java
@@ -5,16 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtils {
-    public static Connection getConnection() throws SQLException {
+
+    static {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Connection getConnection() throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/bands_db";
         String user = "postgres";
         String password = "sacred";
-
         return DriverManager.getConnection(url, user, password);
     }
 }
